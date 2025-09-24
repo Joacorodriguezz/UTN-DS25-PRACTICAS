f4ca2d0eeb36261530175b8bf5ccfdc90e2bd103
Joacorodriguezz <joaco.rodriguez800@gmail.com>
2025-09-23 22:16:45 -0300

    clase 10 - Agrego Boostrap Cards

---
+++ react-ui-lib/src/components/BloqueTema.jsx
@@ -0,0 +1,15 @@
+import '../styles/bloqueTema.css'
+
+function BloqueTema({ title, autor, img }) {
+  return (
+    <div className="card" style={{ width: "18rem" }}>
+        <img src={img} className="card-img-top"/>
+        <div class="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{autor}</p>
+  </div>
+</div>
+  );
+}
+
+export default BloqueTema
